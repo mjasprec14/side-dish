dbea8c2eeb76be1a98d474d3c749561bfce0bcb1
mjasprec14 <michaeljosephasprec1@gmail.com>
2022-10-09 00:24:05 +0800

    feat: folder and file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// eslint-disable-next-line no-unused-vars
+import { Article, Brand, Cta, Feature, Navbar } from "./Components";
+import {
+	Blog,
+	Features,
+	Footer,
+	Header,
+	Possibility,
+	WhatGPT3,
+} from "./Containers";
+
+const App = () => {
+	return (
+		<div className='App'>
+			<div className='gradient__bg'>
+				<Navbar />
+				<Header />
+			</div>
+
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<Cta />
+			<Blog />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
